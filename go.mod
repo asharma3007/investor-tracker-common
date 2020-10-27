0056f4d3591def391f555580025807c4677dfcd6
ankit <ankit@ankits.dev>
2020-10-27 18:46:00 +0000

    first couple types

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/asharma3007/investor-tracker-common
+
+go 1.13
+
+require github.com/shopspring/decimal v1.2.0
--- types.go
+++ types.go
@@ -1,7 +1,21 @@
 package investor_tracker_common
 
+import (
+	. "github.com/shopspring/decimal"
+)
 
-type messageSendEmail struct {
+type Stock struct {
+	Description string
+	Symbol string
+	HlName    string
+	HlUrlOverride string
+	Url       string
+	PriceBuy  Decimal
+	PriceSell Decimal
+}
+
+
+type MessageSendEmail struct {
 	Html string
 	PlainText string
 	SenderName string
