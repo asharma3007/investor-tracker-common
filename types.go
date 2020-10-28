bc05ad3cdd499948af9617a127ef788d9059a8a5
ankit <ankit@ankits.dev>
2020-10-28 22:19:43 +0000

    Migrating watchlist 1

--- types.go
+++ types.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -20,6 +21,7 @@ const (
 	dbname              = "tracker"
 
 	EnvTokenMarketStack = "TOKEN_MARKETSTACK"
+	EnvTokenIex         = "TOKEN_IEX"
 
 	PriceTypeSell = 0
 	PriceTypeBuy = 1
@@ -160,6 +162,107 @@ type Watch struct {
 	Notes		   string
 }
 
+func (wd *WatchDetail) GetPriceLastCloseDesc() string {
+	priceLastClose := wd.GetPriceLastClose()
+	if priceLastClose.IsNegative() { return "No price history"}
+
+	return GetPriceDesc(priceLastClose)
+}
+
+func (w *Watch) GetAlertThresholdDesc() string {
+	return GetPercentDesc(w.AlertThreshold)
+}
+
+func (w *Watch) GetPriceBuyDesc() string {
+	rounded := w.AddedPriceBuy.Round(3)
+	return fmt.Sprintf("%vp", rounded)
+}
+
+type watchDetailIex struct {
+	Stock Stock
+	Watch Watch
+	PriceOpen Decimal `json:"open"`
+	PriceClose Decimal `json:"iexClose"`
+	PriceHigh Decimal `json:"high"`
+	ChangePercent Decimal `json:"changePercent"`
+	Volume Decimal `json:"volume"`
+	AverageVolume Decimal `json:"avgTotalVolume"`
+	PriceBid Decimal `json:"iexBidPrice"`
+	PriceAsk Decimal `json:"iexAskPrice"`
+	PricePreviousClose Decimal `json:"previousClose"`
+	History PriceHistory
+}
+
+func (eod *EodMarketStack) GetPriceCloseDesc() string {
+	return GetPriceDesc(eod.PriceClose)
+}
+
+func GetPriceDesc(price Decimal) string {
+	return fmt.Sprintf("%v", convertPenceToPounds(price))
+}
+
+func (wd *WatchDetail) GetPricePreviousCloseDesc() string {
+	if len(wd.History.Eods) <= 1 {
+		return "No previous"
+	}
+
+	previousDay := wd.History.Eods[1]
+	previousClose := previousDay.PriceClose
+
+	return fmt.Sprintf("%v", convertPenceToPounds(previousClose))
+}
+
+func convertPenceToPounds(price Decimal) Decimal {
+	return price.Mul(NewFromInt(100))
+}
+
+func (wd *WatchDetail) GetChangePercentDesc() string {
+	if !wd.ChangePercent.IsZero() {
+		return GetPercentDesc(wd.ChangePercent)
+	}
+
+	if len(wd.History.Eods) > 1 {
+		lastDay := wd.History.Eods[0]
+		previousDay := wd.History.Eods[1]
+		lastClose := lastDay.PriceClose
+		previousClose := previousDay.PriceClose
+		changePercent := getPercent(lastClose, previousClose)
+		return GetPercentDesc(changePercent)
+	}
+
+	Log(fmt.Sprintf("Could not get percent change from watchdetail %v", wd))
+	return ""
+}
+
+func getPercent(one Decimal, two Decimal) Decimal {
+	change := one.Sub(two)
+	ratio := change.Div(one)
+	return ratio.Mul(NewFromInt(100))
+}
+
+func GetPercentDesc(percent Decimal) string {
+	return fmt.Sprintf("%v %%", percent.Round(3))
+}
+
+const TimeFormatMySql = "2006-01-02 15:04:05"
+const TimeFormatPostGres = time.RFC3339Nano
+
+func (wd *WatchDetail) GetDtReferenceDesc() string {
+	//postgres
+	//parse, err := time.Parse(TimeFormatPostGres, wd.Watch.DtReference)
+	//mysql
+	parse, err := time.Parse(TimeFormatMySql, wd.Watch.DtReference)
+	CheckError(err)
+	return parse.Format(time.RFC822)
+}
+
+func (wd *WatchDetail) GetDeltaReferencePercentDesc() string {
+	priceStartWatch := wd.Watch.AddedPriceBuy
+	priceChange := priceStartWatch.Sub(wd.GetPriceLastClose())
+	percent := getPercent(priceStartWatch, priceChange)
+	return GetPercentDesc(percent)
+}
+
 type PriceHistory struct {
 	Eods []EodMarketStack
 }
@@ -289,7 +392,67 @@ type DefaultHttp struct {
 
 }
 
-func buildWatchDetailMarketStack(client HttpSource, stock *Stock) WatchDetail {
+func BuildWatchDetail(client *DefaultHttp, stock Stock) WatchDetail {
+	if stock.IsSourceHl() {
+		return buildWatchDetailHl(stock)
+		//return watchDetail{}
+	} else {
+		return buildWatchDetailMarketStack(client, stock)
+		//return watchDetail{}
+	}
+}
+
+func buildWatchDetailHl(stock Stock) WatchDetail {
+	fmt.Sprintf("Getting history from HL for %v from %v", stock.ToString(), stock.Url)
+
+	stockPage, err := http.Get(stock.Url)
+	CheckError(err)
+
+	stockDoc, _ := goquery.NewDocumentFromReader(stockPage.Body)
+
+	priceBuyStr, err := stockDoc.Find(".ask.price-divide").Html()
+	CheckError(err)
+	priceSellStr, err := stockDoc.Find(".bid.price-divide").Html()
+	CheckError(err)
+
+	selectionPcChange := stockDoc.Find("span.change-divide > span:nth-child(3)")
+	percentChangeStr, err := selectionPcChange.Html()
+	CheckError(err)
+	percentChangeStr = strings.TrimSpace(percentChangeStr)
+	reg, _ := regexp.Compile("[^0-9.]+")
+	percentChangeStr = reg.ReplaceAllString(percentChangeStr, "")
+
+	isNegative := selectionPcChange.HasClass("negative change")
+	if isNegative { percentChangeStr = "-" + percentChangeStr }
+	percentChange, err := NewFromString(percentChangeStr)
+	CheckError(err)
+
+	if len(priceSellStr) == 0 || len(priceSellStr) == 0 {
+		Log("Failed to get a price for stock")
+		message := fmt.Sprint(stock.Description, " Url: ", stock.Url, " Buy ", priceBuyStr, " Sell ", priceSellStr)
+		Log(message)
+	}
+
+	if len(percentChangeStr) == 0 {
+		Log("Failed to get a percent change for stock")
+		message := fmt.Sprint(stock.Description, " Url: ", stock.Url, " String ", percentChangeStr)
+		Log(message)
+	}
+
+	return WatchDetail{
+		ChangePercent: percentChange,
+		History: PriceHistory{
+			Eods: []EodMarketStack {
+				{
+					Date:       timeMarketStack{time.Now()},
+					PriceClose: parsePrice(priceSellStr),
+				},
+			},
+		},
+	}
+}
+
+func buildWatchDetailMarketStack(client HttpSource, stock Stock) WatchDetail {
 	log := fmt.Sprintf("Getting price history for %v from URL: %v", stock.ToString(), stock.Url)
 	Log(log)
 
@@ -316,6 +479,30 @@ func buildWatchDetailMarketStack(client HttpSource, stock *Stock) WatchDetail {
 	var wd WatchDetail
 	wd.History.Eods = responseDays.Data
 	return wd
+}
+
+func buildWatchDetailIex(stock Stock) WatchDetail {
+	response, err := http.Get(stock.Url)
+	CheckError(err)
+
+	defer response.Body.Close()
+
+	//json.NewDecoder(response.Body).Decode(target)
+	//var str string
+	//decoder := json.NewDecoder(response.Body)
+	//decoder.UseNumber()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	CheckError(err)
+
+	responseString := string(responseData)
+	Log(responseString)
+
+	var quote WatchDetail
+	err = json.Unmarshal(responseData, &quote)
+	CheckError(err)
+
+	return quote
 }
 
 func (stock *Stock) populateFromHl() {
@@ -366,7 +553,12 @@ func parsePrice(priceStr string) Decimal {
 
 func (stock *Stock) populateFromMarketStack() {
 	var httpClient DefaultHttp
-	watchDetail := buildWatchDetailMarketStack(&httpClient, stock)
+	watchDetail := buildWatchDetailMarketStack(&httpClient, *stock)
 	stock.PriceBuy = watchDetail.GetPriceLastClose()
 	stock.PriceSell = watchDetail.GetPriceLastClose()
 }
+
+func getIexUrl(stock Stock) string {
+	return fmt.Sprintf("https://cloud.iexapis.com/stable/stock/%v/quote?token=%v", stock.Symbol, os.Getenv(EnvTokenIex))
+}
+
